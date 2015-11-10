9bf3bcaa713e7e2dc15e5da22769d372addf113c
Donna Peplinskie <donna.peplinskie@risevision.com>
2015-11-10 14:22:44 -0500

    Fix how throttling is detected

--- test/js/rise-storage-data.js
+++ test/js/rise-storage-data.js
@@ -5,28 +5,32 @@ var header = { "Content-Type": "text/json" },
       "contentType": "text/plain",
       "updated": "2015-02-04T17:44:00.549Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2F",
-      "etag": "CIjxrtzryMMCEAE="
+      "etag": "CIjxrtzryMMCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/home.jpg",
       "contentType": "image/jpeg",
       "updated": "2015-02-04T17:45:25.945Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fhome.jpg",
-      "etag": "COjLvarr/cQCEAE="
+      "etag": "COjLvarr/cQCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/circle.png",
       "contentType": "image/png",
       "updated": "2015-02-06T14:25:11.312Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fcircle.png",
-      "etag": "CMiEudSn2MMCEAs="
+      "etag": "CMiEudSn2MMCEAs=",
+      "isThrottled": false
     },
     {
       "name": "images/my-image.bmp",
       "contentType": "image/bmp",
       "updated": "2015-02-06T11:24:13.313Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fmy-image.bmp",
-      "etag": "CJi3zten2MMCEA0="
+      "etag": "CJi3zten2MMCEA0=",
+      "isThrottled": false
     }]
   },
   mixedMedia = {
@@ -35,63 +39,72 @@ var header = { "Content-Type": "text/json" },
       "contentType": "text/plain",
       "updated": "2015-02-04T17:44:00.549Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2F",
-      "etag": "CIjxrtzryMMCEAE="
+      "etag": "CIjxrtzryMMCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/home.jpg",
       "contentType": "image/jpeg",
       "updated": "2015-02-04T17:45:25.945Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fhome.jpg",
-      "etag": "COjLvarr/cQCEAE="
+      "etag": "COjLvarr/cQCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/circle.png",
       "contentType": "image/png",
       "updated": "2015-02-06T14:25:11.312Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fcircle.png",
-      "etag": "CMiEudSn2MMCEAs="
+      "etag": "CMiEudSn2MMCEAs=",
+      "isThrottled": false
     },
     {
       "name": "images/my-image.bmp",
       "contentType": "image/bmp",
       "updated": "2015-02-06T11:24:13.313Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fmy-image.bmp",
-      "etag": "CJi3zten2MMCEA0="
+      "etag": "CJi3zten2MMCEA0=",
+      "isThrottled": false
     },
     {
       "name": "images/golf.svg",
       "contentType": "image/svg+xml",
       "updated": "2015-01-30T08:19:09.263Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fgolf.svg",
-      "etag": "CPjC1qHc7MQCEAE="
+      "etag": "CPjC1qHc7MQCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/turtle.gif",
       "contentType": "image/gif",
       "updated": "2015-02-04T17:46:31.263Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fturtle.gif",
-      "etag": "CLCKypbW/cQCEAE="
+      "etag": "CLCKypbW/cQCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/car-ad.mp4",
       "contentType": "video/mp4",
       "updated": "2015-02-02T10:03:11.263Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fcar-ad.mp4",
-      "etag": "CKCa+Y2nq8MCEAM="
+      "etag": "CKCa+Y2nq8MCEAM=",
+      "isThrottled": false
     },
     {
       "name": "images/walking-dead.ogv",
       "contentType": "video/ogg",
       "updated": "2015-02-01T09:08:15.263Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fwalking-dead.ogv",
-      "etag": "CJC2jJrFqsMCEAE="
+      "etag": "CJC2jJrFqsMCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/south-park.webm",
       "contentType": "video/webm",
       "updated": "2015-02-03T19:13:45.263Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fsouth-park.webm",
-      "etag": "CLDO+JDspcMCEAE="
+      "etag": "CLDO+JDspcMCEAE=",
+      "isThrottled": false
     }]
   },
   folderImage = {
@@ -100,7 +113,8 @@ var header = { "Content-Type": "text/json" },
       "contentType": "image/jpeg",
       "updated": "2015-02-04T17:45:25.945Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fhome.jpg",
-      "etag": "COjLvarr/cQCEAE="
+      "etag": "COjLvarr/cQCEAE=",
+      "isThrottled": false
     }]
   },
   bucketImage = {
@@ -116,6 +130,7 @@ var header = { "Content-Type": "text/json" },
         "dateOnly": false,
         "timeZoneShift": 0
       },
+      "isThrottled": false
     }],
     "bucketExists": true,
     "kind": "storage#filesItem",
@@ -127,35 +142,40 @@ var header = { "Content-Type": "text/json" },
       "contentType": "text/plain",
       "updated": "2015-02-04T17:44:00.549Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2F",
-      "etag": "CIjxrtzryMMCEAE="
+      "etag": "CIjxrtzryMMCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/home.jpg",
       "contentType": "image/jpeg",
       "updated": "2015-02-04T17:45:25.945Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fhome.jpg",
-      "etag": "COjLvarr/cQCEAE="
+      "etag": "COjLvarr/cQCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/subfolder1/",
       "contentType": "text/plain",
       "updated": "2015-02-04T17:44:00.549Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fsubfolder1%2F",
-      "etag": "CIjxrtzryMMCEAE="
+      "etag": "CIjxrtzryMMCEAE=",
+      "isThrottled": false
     },
     {
       "name": "images/circle.png",
       "contentType": "image/png",
       "updated": "2015-02-06T14:25:11.312Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fcircle.png",
-      "etag": "CMiEudSn2MMCEAs="
+      "etag": "CMiEudSn2MMCEAs=",
+      "isThrottled": false
     },
     {
       "name": "images/subfolder2/",
       "contentType": "text/plain",
       "updated": "2015-02-04T17:44:00.549Z",
       "selfLink": "https://www.googleapis.com/storage/v1/b/risemedialibrary-abc123/o/images%2Fsubfolder2%2F",
-      "etag": "CIjxrtzryMMCEAE="
+      "etag": "CIjxrtzryMMCEAE=",
+      "isThrottled": false
     }]
   },
   noFolder = {
@@ -177,7 +197,8 @@ var header = { "Content-Type": "text/json" },
         "value": "1442341739897",
         "dateOnly": false,
         "timeZoneShift": 0
-      }
+      },
+      "isThrottled": false
     }],
     "bucketExists": true,
     "kind": "storage#filesItem",
